313bf86f07855e9a29cc493e99eeb3a652edf02a
ISlam SAleh <iamislam.fsd@gmail.com>
2023-03-16 06:24:26 -0700

    puts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,6 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of a int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
