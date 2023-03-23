2b4886335d867f34942506ffd07a4aa2fba96c07
ISlam SAleh <iamislam.fsd@gmail.com>
2023-03-23 03:21:50 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * positive_or_negative eck number if integer is positive or negative
+ *
+ * @i: the number to  be checked
+ *
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+if (i < 0)
+printf("%d is positive", i);
+else if (i > 0)
+printf("%d is negative", i);
+}
